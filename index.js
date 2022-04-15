d191a570103661426f543c8234b72d8677a857fc
mitya-me <dmitrymefodyev@gmail.com>
2022-04-16 01:44:58 +0300

    Add nodemon and users router

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 import express from 'express';
+import { userRouter } from './users/users.js';
 
 const port = 3000;
 const app = express();
+const timeIsNow = new Date().toLocaleString();
+
+app.use((req, res, next) => { 
+	console.log(`[${timeIsNow}] ${req.method}: ${req.url}`)
+	next();
+})
 
 app.get('/hello', (req, res) => {
-	res.send('Hello people')
+	res.end()
+})
+
+app.use('/users', userRouter);
+
+app.use(err => { 
+	console.log(err.message) 
+	res.status(500).send(`Oops... ${error.message}`)
 })
 
 app.listen(port, (err) => { 
 	if (err) console.log('Ooops.. Damned!', err)
-	console.log(`::: START ::: Success: server started. http://localhost:${port}`)
+	console.log(`[${timeIsNow}] http://localhost:${port}`)
 })
-
-~
---
+++ users/users.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+
+const userRouter = express.Router();
+
+userRouter.use((req, res, next) => { 
+	console.log('test middleware in userRouter')
+	next();
+})
+
+userRouter.post('/login', (req, res) => {
+	res.send('login')
+})
+
+userRouter.post('/register', (req, res) => {
+	res.send('register')
+})
+
+
+
+export { userRouter }
