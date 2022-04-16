c763fa33ded77fc87f3eba347ca4f7f8455715da
mitya-me <dmitrymefodyev@gmail.com>
2022-04-16 23:16:58 +0300

    Add in app userController

--- src/common/route.interface.ts
+++ src/common/route.interface.ts
@@ -1,8 +1,8 @@
-import { Request, Response, Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 
 export interface IControllerRoute { 
 	path: string;
-	func: (req: Request, res: Response) => void;
+	func: (req: Request, res: Response, next: NextFunction) => void;
 	method: keyof Pick<Router, 'get' | 'post' | 'delete' | 'patch' | 'put'>;
 	// method: 'get' | 'post' | 'delete' | 'patch' | 'put';
  }
