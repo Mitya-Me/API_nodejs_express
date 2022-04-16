7a6a573a0f46773dbf124feb31d4806a51640825
mitya-me <dmitrymefodyev@gmail.com>
2022-04-16 23:45:07 +0300

    Add in app exceptionfilter and extended class HTTPError

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, Response } from "express";
 import { BaseController } from "../common/base.controller";
 import { LoggerService } from "../logger/logger.service";
 
-
 export class UserController extends BaseController {
 	constructor(logger: LoggerService) { 
 		super(logger)
